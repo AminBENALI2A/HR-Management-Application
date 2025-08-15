aeb15b28c2f4503cbe9b82b41049f7f63c3f5801
AminBENALI2A <aminebenaliroo@gmail.com>
2025-08-15 20:15:54 +0100

    v1.82

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { Users } from './entities/Users';
 import { UsersModule } from './modules/users/users.module';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 @Module({
   imports: [ScheduleModule.forRoot(),
@@ -19,6 +21,15 @@ import { UsersModule } from './modules/users/users.module';
       password: process.env.DB_PASSWORD || 'adminpassword',
       database: process.env.DB_NAME || 'hr_management',
       entities: [__dirname + '/entities/*.js'],  // use .ts if running ts-node locally
+      ssl: {
+        ca: readFileSync("../global-bundle.pem"),
+        rejectUnauthorized: true
+      },
+      extra: {
+        ssl: {
+          require: true,
+        }
+      },
       synchronize: false,  // keep false in production
       logging: true,
     }),
