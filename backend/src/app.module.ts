a6681b092e2bd45a39724e1e8c200387a83e9230
AminBENALI2A <aminebenaliroo@gmail.com>
2025-08-15 20:53:33 +0100

    Please

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,26 +3,27 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import { Users } from './entities/Users';
 import { UsersModule } from './modules/users/users.module';
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
 @Module({
-  imports: [ScheduleModule.forRoot(),
+  imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env', // Explicitly specify the env file
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USER || 'admin',
-      password: process.env.DB_PASSWORD || 'adminpassword',
+      host: process.env.DB_HOST || 'hr-management-optymum.cqbu0goc27gf.us-east-1.rds.amazonaws.com',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER || 'hrmanagement',
+      password: process.env.DB_PASSWORD || 'hrmanagement',
       database: process.env.DB_NAME || 'hr_management',
-      entities: [__dirname + '/entities/*.js'],  // use .ts if running ts-node locally
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       ssl: {
-        ca: readFileSync("../global-bundle.pem"),
+        ca: readFileSync(join(__dirname, 'global-bundle.pem')).toString(),
         rejectUnauthorized: true
       },
       extra: {
@@ -30,7 +31,7 @@ import { readFileSync } from 'fs';
           require: true,
         }
       },
-      synchronize: false,  // keep false in production
+      synchronize: false,
       logging: true,
     }),
     AuthModule,
