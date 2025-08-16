a3180cfe95339cb41d99e875cae97f623f049c3b
AminBENALI2A <aminebenaliroo@gmail.com>
2025-08-16 11:08:35 +0100

    jh

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -46,7 +46,7 @@ const Auth: React.FC = () => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
       });*/
-      const url = "http://hroptenv.eba-xunhtune.us-east-1.elasticbeanstalk.com/auth/health";
+      const url = "https://d1pc059cxwtfw0.cloudfront.net/auth/health";
       console.log(url);
       const response = await fetch(url, {
         method: 'GET',
